6913789644440fd80ec16585ad23e30c1b39023d
gkrdbsrla <gkrdbsrla@naver.com>
2024-04-26 16:22:50 +0900

    feat : custom login

---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        // permitAll     : 모든 사용자 접근가능
+        // hasRole       : 특정한 규칙이 있어야 접근가능
+        // authenticated : 로그인만 진행하면 접근가능
+        // denyAll       : 로그인해도 접근불가능
+        // 동작되는 순서는 상단부터 동작되기 때문에 순서를 잘 유의해서 작성해야함
+        // 가장 아래에서 모든 경로에 대한 세팅을 진행해야함
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
+
+        http
+                .formLogin((auth) -> auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
+                );
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        return http.build();
+    }
+
+}
