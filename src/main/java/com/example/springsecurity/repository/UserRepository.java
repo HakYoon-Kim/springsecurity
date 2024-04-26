0009d67f0bedf0316b9376f44ba584eb2882a28d
gkrdbsrla <gkrdbsrla@naver.com>
2024-04-26 18:00:33 +0900

    feat : 로그인 검증

--- src/main/java/com/example/springsecurity/repository/UserRepository.java
+++ src/main/java/com/example/springsecurity/repository/UserRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
+    boolean existsByUsername(String username);
+
+    UserEntity findByUsername(String username);
+
 }
